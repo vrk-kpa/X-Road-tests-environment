6ec015104724bf644cf8ec9c83444989189c5336
Lasse Matikainen <lasse.matikainen@qautomate.fi>
2018-05-24 12:42:15 +0300

    Update install_pre_setup.sh

--- harmonized-test-environment/install_pre_setup.sh
+++ harmonized-test-environment/install_pre_setup.sh
@@ -3,8 +3,8 @@
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get -y install lxd
-sudo apt-get install python
-sudo apt-get install python-pip
+sudo apt-get -y install python
+sudo apt-get -y install python-pip
 python -m pip install 'ansible==2.5.0' 
 echo "Run LXD init"
 sudo service lxd start
