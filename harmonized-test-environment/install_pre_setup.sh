390e42f1c65e84e59bc1b49c01eb8a63a98a362e
lasse <lasse.matikainen@qautomate.fi>
2018-05-24 12:15:56 +0300

    change harmonized-test script ansible install

--- harmonized-test-environment/install_pre_setup.sh
+++ harmonized-test-environment/install_pre_setup.sh
@@ -2,8 +2,10 @@
 # Install Ansible
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
-sudo apt-get -y install ansible
 sudo apt-get -y install lxd
+sudo apt-get install python
+sudo apt-get install python-pip
+python -m pip install 'ansible==2.5.0' 
 echo "Run LXD init"
 sudo service lxd start
 sudo lxd init
